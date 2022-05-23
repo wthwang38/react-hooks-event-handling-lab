e9496591106d698246c4244fa23834f295374448
William Hwang <willhwang90@gmail.com>
2022-05-23 12:03:15 -0400

    commit tree d525ad62b871aa4bf7ee229ead1ec36585157904

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,12 @@
 // Code EyesOnMe Component Here
+import React from 'react'
+const handleFocus = ()=> (console.log("Good!"))
+const handleBlur = ()=>(console.log('Hey! Eyes on me!'))
+export const EyesOnMe = () => {
+  return (
+    <div>
+        <button onFocus={handleFocus} onBlur={handleBlur}>Eyes on me</button>
+    </div>
+  )
+}
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,11 @@
 // Code Keypad Component Here
+import React from 'react'
+const handleInput = ()=> (console.log('Entering password...'))
+export const Keypad = () => {
+  return (
+    <div>Keypad
+        <input type="password" onChange={handleInput}/>
+     </div>
+  )
+}
+export default Keypad;
